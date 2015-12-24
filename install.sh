fb8ee76d07a5b46d8c6686150f1cd9ee1435328c
antonokulov <qantus@mail.ru>
2015-12-24 16:57:24 +0300

    Up meta and install sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ git clone https://github.com/studio107/Core.git
 git clone https://github.com/studio107/Files.git
 git clone https://github.com/studio107/Mail.git
 git clone https://github.com/studio107/Menu.git
-git clone https://github.com/studio107/Meta.git
+git clone https://github.com/qantus/Meta.git
 git clone https://github.com/studio107/Pages.git
 git clone https://github.com/studio107/Redirect.git
 git clone https://github.com/studio107/Sitemap.git
